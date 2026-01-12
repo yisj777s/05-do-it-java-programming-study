b677834d445396f476005f6e71907c23d8a7edf4
yisj777s <yisj777s@naver.com>
2026-01-12 19:49:22 +0900

    feat(practice-02): Q04 변수 합산 결과 출력

---
+++ Chapter2/src/chapter2/practice/Q04_AddIntAndDouble.java
@@ -0,0 +1,11 @@
+package chapter2.practice;
+
+public class Q04_AddIntAndDouble {
+    public static void main(String[] args) {
+        int iNum1 = 10;
+        double dNum = 2.0;
+        int iNum2 = (int)(iNum1 + dNum);
+
+        System.out.println(iNum2);
+    }
+}
