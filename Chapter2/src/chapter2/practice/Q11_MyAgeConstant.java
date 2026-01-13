5109dcb698857f935b0e95c04d87dcc91461b4bd
yisj777s <yisj777s@naver.com>
2026-01-13 19:45:21 +0900

    feat(practice-02): Q11 상수 MY_AGE 선언 및 출력

---
+++ Chapter2/src/chapter2/practice/Q11_MyAgeConstant.java
@@ -0,0 +1,8 @@
+package chapter2.practice;
+
+public class Q11_MyAgeConstant {
+    public static void main(String[] args) {
+        final int MY_AGE = 22;      // 상수 MY_AGE를 선언하고 값 22를 대입함
+        System.out.println(MY_AGE); // MY_AGE값을 출력함
+    }
+}
