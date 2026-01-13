4ba05d130c9944fb6cc98c200dd5f9f778576150
yisj777s <yisj777s@naver.com>
2026-01-13 19:11:44 +0900

    feat(practice-02): Q05 문자 코드 출력 확인

---
+++ Chapter2/src/chapter2/practice/Q05_CharUnicode.java
@@ -0,0 +1,8 @@
+package chapter2.practice;
+
+public class Q05_CharUnicode {
+    public static void main(String[] args) {
+        char ch = '\uAE00'; // '글' 문자 유니코드 값
+        System.out.println(ch);
+    }
+}
