0961bec709033f87ec1acc7a81d35a299607d6e0
yisj777s <yisj777s@naver.com>
2026-01-13 19:19:18 +0900

    feat(practice-02): Q06 나이(age) 변수 선언 및 출력

---
+++ Chapter2/src/chapter2/practice/Q06_AgeVariable.java
@@ -0,0 +1,9 @@
+package chapter2.practice;
+
+public class Q06_AgeVariable {
+    public static void main(String[] args) {
+        int age;
+        age = 29;
+        System.out.println(age);
+    }
+}
