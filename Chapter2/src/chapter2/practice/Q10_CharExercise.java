c1ff2c286994aaed841dbbe227ab9c65d070a9da
yisj777s <yisj777s@naver.com>
2026-01-13 19:37:35 +0900

    feat(practice-02): Q10 문자형 값 변경 출력

---
+++ Chapter2/src/chapter2/practice/Q10_CharExercise.java
@@ -0,0 +1,16 @@
+package chapter2.practice;
+
+public class Q10_CharExercise {
+    public static void main(String[] args) {
+        char ch1 = 'Z';
+        System.out.println(ch1); //문자 출력
+        System.out.println((int)ch1); //문자에 해당하는 정수값(아스키 코드값) 출력
+
+        char ch2 = 38; //정숫값 대입
+        System.out.println(ch2); //정수값에 해당하는 문자 출력
+
+        int ch3 = 97;
+        System.out.println(ch3); // 문자 정수값 출력
+        System.out.println((char)ch3); // 정수값에 해당하는 문자 출력
+    }
+}
