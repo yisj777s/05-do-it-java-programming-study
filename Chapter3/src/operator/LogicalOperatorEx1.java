ec3aaec69c1ece9be5d5f14f35cd34237776ec0d
yisj777s <yisj777s@naver.com>
2026-01-20 19:00:16 +0900

    feat(03-1): 논리 연산자 예제 작성

---
+++ Chapter3/src/operator/LogicalOperatorEx1.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class LogicalOperatorEx1 {
+    public static void main (String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        boolean flag = (num1 > 0) && (num2 > 0);
+        System.out.println(flag); // true
+
+        flag = (num1 < 0) && (num2 > 0);
+        System.out.println(flag); // false
+
+        flag = (num1 < 0) || (num2 > 0);
+        System.out.println(flag); // true
+    }
+}
