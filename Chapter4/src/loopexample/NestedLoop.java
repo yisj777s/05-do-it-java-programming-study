70d9bb07c9f6514cbeaab29cdba15700f2cfcecc
yisj777s <yisj777s@naver.com>
2026-02-11 01:01:15 +0900

    feat(04-2): 중첩 for문으로 구구단 출력 예제 작성

---
+++ Chapter4/src/loopexample/NestedLoop.java
@@ -0,0 +1,18 @@
+package loopexample;
+
+/**
+ * 중첩된 반복문으로 구구단 계산하기
+ */
+public class NestedLoop {
+    public static void main(String[] args) {
+        int dan;
+        int times;
+
+        for (dan = 2; dan <= 9; dan++) {
+            for (times = 1; times <= 9; times++) {
+                System.out.println(dan + "X" + times + "=" + dan * times);
+            }
+            System.out.println(); // 한 줄 띄워서 출력
+        }
+    }
+}
