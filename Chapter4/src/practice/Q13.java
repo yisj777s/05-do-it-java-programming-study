5e3e985d98c10b4f20ce66420de7e2aa72df685f
yisj777s <yisj777s@naver.com>
2026-02-24 19:27:16 +0900

    feat(04): 되새김 문제 Q13 for문으로 안녕하세요1~10 출력

---
+++ Chapter4/src/practice/Q13.java
@@ -0,0 +1,12 @@
+package practice;
+
+/**
+ * for문으로 안녕하세요1~10 출력
+ */
+public class Q13 {
+    public static void main(String[] args) {
+        for (int num = 1; num <= 10; num++) {
+            System.out.println("안녕하세요" + num);
+        }
+    }
+}
