5a2a194c260912335d08a2bff6511ddc31a37783
yisj777s <yisj777s@naver.com>
2026-03-09 19:46:25 +0900

    feat(05-4): Student 클래스에 main() 함수 추가하기

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -10,7 +10,11 @@ public class Student {
         return studentName;
     }
 
-    public void setStudentName(String name) {
-        studentName = name;
+    public static void main(String[] args) {
+        Student studentAhn = new Student();
+        studentAhn.studentName = "안연수";
+
+        System.out.println(studentAhn.studentName);
+        System.out.println(studentAhn.getStudentName());
     }
 }
