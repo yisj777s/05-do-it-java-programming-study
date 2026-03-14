9535ebb50a52972f0ff3e1ca7cf3631fe103905a
yisj777s <yisj777s@naver.com>
2026-03-14 15:37:30 +0900

    feat(05-5): 생성자 추가

--- Chapter5/src/constructor/Person.java
+++ Chapter5/src/constructor/Person.java
@@ -11,4 +11,10 @@ public class Person {
     public Person(String name) {
         this.name = name;
     }
+
+    public Person(String name, float height, float weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
 }
