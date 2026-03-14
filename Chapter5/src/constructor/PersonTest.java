ab24dd039f4f8d37bd2d2350f960f2d13a1efbb0
yisj777s <yisj777s@naver.com>
2026-03-14 18:25:07 +0900

    feat(05-5): 객체 생성 및 생성자 호출 테스트

--- Chapter5/src/constructor/PersonTest.java
+++ Chapter5/src/constructor/PersonTest.java
@@ -2,6 +2,11 @@ package constructor;
 
 public class PersonTest {
     public static void main(String[] args) {
-        Person personLee = new Person();
+        Person personKim = new Person();
+        personKim.name = "김유신";
+        personKim.weight = 85.5F;
+        personKim.height = 180.0F;
+
+        Person personYi = new Person("이순신", 175, 75);
     }
 }
