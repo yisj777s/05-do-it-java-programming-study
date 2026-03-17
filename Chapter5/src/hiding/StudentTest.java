e07ada6eb5145a9c2d15d96adb4c32032d0d3404
yisj777s <yisj777s@naver.com>
2026-03-17 19:45:55 +0900

    feat(05-7): private 변수 테스트하기

---
+++ Chapter5/src/hiding/StudentTest.java
@@ -0,0 +1,10 @@
+package hiding;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student studentLee = new Student();
+        studentLee.studentName = "이상원";
+
+        System.out.println(studentLee.getStudentName());
+    }
+}
