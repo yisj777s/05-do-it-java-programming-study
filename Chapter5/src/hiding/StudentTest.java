a45f28685376c195274ad1c208d586405109d125
yisj777s <yisj777s@naver.com>
2026-03-18 18:19:02 +0900

    feat(05-7): private 변수에 접근하기

--- Chapter5/src/hiding/StudentTest.java
+++ Chapter5/src/hiding/StudentTest.java
@@ -3,7 +3,8 @@ package hiding;
 public class StudentTest {
     public static void main(String[] args) {
         Student studentLee = new Student();
-        studentLee.studentName = "이상원";
+        //studentLee.studentName = "이상원";
+        studentLee.setStudentName("이상원");
 
         System.out.println(studentLee.getStudentName());
     }
