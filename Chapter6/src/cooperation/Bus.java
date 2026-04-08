7ed519e4498a23f901c35aac659f1f9b0643bda3
yisj777s <yisj777s@naver.com>
2026-04-08 19:17:38 +0900

    feat(06-2): Bus 클래스 작성

---
+++ Chapter6/src/cooperation/Bus.java
@@ -0,0 +1,22 @@
+package cooperation;
+
+public class Bus {
+    private int busNumber;
+    private int passengerCount; // 승객 수
+    private int money; // 버스 수입
+
+    public Bus(int busNumber) {
+        this.busNumber = busNumber;
+    }
+
+    public void take(int money) {
+        this.money += money;
+        passengerCount++;
+    }
+
+    public void showInfo() {
+        System.out.println("버스 " + busNumber + "번의 승객은 " + passengerCount +
+                "명이고, 수입은 " + money + "입니다."
+        );
+    }
+}
