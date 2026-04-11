dcc69aef434224ae934116856ca39e1464012a37
yisj777s <yisj777s@naver.com>
2026-04-11 17:46:44 +0900

    feat(06-3): static 변수 활용 학번 생성 - Student1 클래스 작성

---
+++ Chapter6/src/staticex/Student1.java
@@ -0,0 +1,22 @@
+package staticex;
+
+public class Student1 {
+    static int serialNum = 1000;
+    private int studentID;
+    private String studentName;
+    private int grade;
+    private String address;
+
+    public Student1() {
+        serialNum++;
+        studentID = serialNum;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String name) {
+        this.studentName = name;
+    }
+}
