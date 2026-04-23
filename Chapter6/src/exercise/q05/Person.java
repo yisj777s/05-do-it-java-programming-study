b9495ad99cf051c277f33e74b2cff3ef642d7e7a
yisj777s <yisj777s@naver.com>
2026-04-23 19:17:32 +0900

    wip(06): 되새김 문제 Q05 Person 클래스 작성 중

---
+++ Chapter6/src/exercise/q05/Person.java
@@ -0,0 +1,15 @@
+package exercise.q05;
+
+public class Person {
+    private String name;
+    private int money;
+
+    public Person(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public void ShowInfo() {
+        System.out.println("아침 출근길에 " + name + "씨는 " + cafe.)
+    }
+}
