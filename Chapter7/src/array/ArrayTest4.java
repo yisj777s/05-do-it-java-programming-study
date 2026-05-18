07e0a19e4776f009805e86c273ef7974b4a580d1
yisj777s <yisj777s@naver.com>
2026-05-18 15:33:00 +0900

    feat(07): 알파벳 문자와 아스키 코드값 출력

---
+++ Chapter7/src/array/ArrayTest4.java
@@ -0,0 +1,16 @@
+package array;
+
+public class ArrayTest4 {
+    public static void main(String[] args) {
+        char[] alphabets = new char[26];
+        char ch = 'A';
+
+        for (int i = 0; i < alphabets.length; i++, ch++) {
+            alphabets[i] = ch;
+        }
+
+        for (int i = 0; i < alphabets.length; i++) {
+            System.out.println(alphabets[i] + "," + (int) alphabets[i]);
+        }
+    }
+}
