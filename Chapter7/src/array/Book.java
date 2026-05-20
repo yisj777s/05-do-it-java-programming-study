2507a3ccf0cf7571d6f6d024bb6335c8d8e805d0
yisj777s <yisj777s@naver.com>
2026-05-20 19:41:02 +0900

    feat(07-2): 객체 배열 깊은 복사

--- Chapter7/src/array/Book.java
+++ Chapter7/src/array/Book.java
@@ -4,6 +4,8 @@ public class Book {
     private String bookName;
     private String author;
 
+    public Book() {}
+
     public Book(String bookName, String author) {
         this.bookName = bookName;
         this.author = author;
