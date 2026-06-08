82107f49ed675d0b23e950079e2637f3ed05fe69
yisj777s <yisj777s@naver.com>
2026-06-08 19:54:37 +0900

    feat(08-1): Customer 필드 protected 변경 및 getter/setter 추가

--- Chapter8/src/inheritance/Customer.java
+++ Chapter8/src/inheritance/Customer.java
@@ -1,11 +1,11 @@
 package inheritance;
 
 public class Customer {
-    private int customerId;
-    private String customerName;
-    private String customerGrade;
-    private int bonusPoint;
-    private double bonusRatio;
+    protected int customerId;
+    protected String customerName;
+    protected String customerGrade;
+    protected int bonusPoint;
+    protected double bonusRatio;
 
     public Customer() {
         customerGrade = "SILVER";
@@ -19,5 +19,29 @@ public class Customer {
 
     public String showCustomerInfo() {
         return customerName + " 님의 등급은 " + customerGrade + "이며, 보너스 포인트는 " + bonusPoint + "입니다.";
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerGrade() {
+        return customerGrade;
+    }
+
+    public void setCustomerGrade(String customerGrade) {
+        this.customerGrade = customerGrade;
     }
 }
