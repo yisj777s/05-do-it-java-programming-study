a91073a73da67d13fb02b100d03661743daaaac6
yisj777s <yisj777s@naver.com>
2026-06-10 19:24:39 +0900

    feat(08-2): VIPCustomer 생성자 호출 로그 추가

--- Chapter8/src/inheritance/VIPCustomer.java
+++ Chapter8/src/inheritance/VIPCustomer.java
@@ -8,6 +8,7 @@ public class VIPCustomer extends Customer {
         customerGrade = "VIP";
         bonusRatio = 0.05;
         saleRatio = 0.1;
+        System.out.println("VIPCustomer() 생성자 호출");
     }
 
     public int getAgentId() {
