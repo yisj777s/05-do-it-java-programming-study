74df1014e175c7cfe0597172e7e28cd62c6a527f
yisj777s <yisj777s@naver.com>
2026-06-12 15:48:26 +0900

    feat(08-2): VIPCustomer 클래스 생성자 추가, 명시적으로 상위 클래스 생성자 호출 2

--- Chapter8/src/inheritance/CustomerTest2.java
+++ Chapter8/src/inheritance/CustomerTest2.java
@@ -2,9 +2,9 @@ package inheritance;
 
 public class CustomerTest2 {
     public static void main(String[] args) {
-        VIPCustomer customerKim = new VIPCustomer(); // 하위 클래스 생성
-        customerKim.setCustomerId(10020);
-        customerKim.setCustomerName("김유신");
+        VIPCustomer customerKim = new VIPCustomer(10020, "김유신", 20010); // 하위 클래스 생성
+//        customerKim.setCustomerId(10020);
+//        customerKim.setCustomerName("김유신");
         customerKim.bonusPoint = 10000;
         System.out.println(customerKim.showCustomerInfo());
     }
