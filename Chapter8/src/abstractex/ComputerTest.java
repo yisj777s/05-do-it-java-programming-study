23c65ce3f0a2a8499f92acfdc46eb91487b1bc43
yisj777s <yisj777s@naver.com>
2026-07-06 18:21:18 +0900

    feat(09-1): 추상 클래스 객체 생성 테스트

---
+++ Chapter8/src/abstractex/ComputerTest.java
@@ -0,0 +1,10 @@
+package abstractex;
+
+public class ComputerTest {
+    public static void main(String[] args) {
+        Computer computer1 = new Computer();
+        Computer computer2 = new DeskTop();
+        Computer computer3 = new NoteBook();
+        Computer computer4 = new MyNoteBook();
+    }
+}
