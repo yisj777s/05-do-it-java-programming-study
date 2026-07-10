ca7b9eff160c48166244f8627e56b8ca4b488912
yisj777s <yisj777s@naver.com>
2026-07-10 12:15:23 +0900

    feat(09-2): CarTest 클래스 구현 및 템플릿 메서드 실행 테스트

--- Chapter9/src/abstractex/ComputerTest.java
+++ Chapter9/src/abstractex/ComputerTest.java
@@ -1,10 +1,10 @@
-package abstractex;
-
-public class ComputerTest {
-    public static void main(String[] args) {
-        Computer computer1 = new Computer();
-        Computer computer2 = new DeskTop();
-        Computer computer3 = new NoteBook();
-        Computer computer4 = new MyNoteBook();
-    }
-}
+//package abstractex;
+//
+//public class ComputerTest {
+//    public static void main(String[] args) {
+//        Computer computer1 = new Computer();
+//        Computer computer2 = new DeskTop();
+//        Computer computer3 = new NoteBook();
+//        Computer computer4 = new MyNoteBook();
+//    }
+//}
