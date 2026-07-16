9fa33d19b99ae19feaf7885c14315d46fe45a08e
yisj777s <yisj777s@naver.com>
2026-07-16 22:02:18 +0900

    feat(09-4): 공용 사용을 위한 static final 상수 정의

---
+++ Chapter9/src/finalex/Constants.java
@@ -0,0 +1,10 @@
+package finalex;
+
+public class Constants {
+    public static final int MIN = 1;
+    public static final int MAX = 99999;
+    public static final int ENGLISH = 1001;
+    public static final int MATH = 2001;
+    public static final double PI = 3.14;
+    public static final String GOOD_MORNING = "Good Morning!";
+}
