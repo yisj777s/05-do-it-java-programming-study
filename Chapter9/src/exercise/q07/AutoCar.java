e576307d70da3e85d395ee4a21b9ea210a860805
yisj777s <yisj777s@naver.com>
2026-07-20 20:42:26 +0900

    feat(09): 되새김문제 Q07 AutoCar 클래스 구현

---
+++ Chapter9/src/exercise/q07/AutoCar.java
@@ -0,0 +1,17 @@
+package exercise.q07;
+
+public class AutoCar extends Car {
+    @Override
+    public void run() {
+        System.out.println("자동차가 달립니다.");
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println("휘발유를 주유합니다.");
+    }
+
+    public void load() {
+        System.out.println("짐을 싣습니다.");
+    }
+}
