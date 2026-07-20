5810543f18ba9c422a0808a928fe374e5f6244b3
yisj777s <yisj777s@naver.com>
2026-07-20 20:37:31 +0900

    feat(09): 되새김문제 Q07 Car 추상 클래스 정의

---
+++ Chapter9/src/exercise/q07/Car.java
@@ -0,0 +1,11 @@
+package exercise.q07;
+
+public abstract class Car {
+    public abstract void run();
+
+    public abstract void refuel();
+
+    public void stop() {
+        System.out.println("차가 멈춥니다.");
+    }
+}
