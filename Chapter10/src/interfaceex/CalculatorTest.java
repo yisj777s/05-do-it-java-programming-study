c9a4fda7601c13c0bb5fb62c52d8fca9b36c780c
yisj777s <yisj777s@naver.com>
2026-07-30 18:18:46 +0900

    feat(10-1): CalculatorTest 사칙연산 테스트

---
+++ Chapter10/src/interfaceex/CalculatorTest.java
@@ -0,0 +1,15 @@
+package interfaceex;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        CompleteCalculator calculator = new CompleteCalculator();
+        System.out.println(calculator.add(num1, num2));
+        System.out.println(calculator.substract(num1, num2));
+        System.out.println(calculator.multiply(num1, num2));
+        System.out.println(calculator.divide(num1, num2));
+        calculator.showInfo();
+    }
+}
