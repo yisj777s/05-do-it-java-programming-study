c125d7d5bd5a1bf89f883edd5d52bb882f49a2f9
yisj777s <yisj777s@naver.com>
2026-09-10 20:34:17 +0900

    feat(10): 되새김 문제 Q08 BubbleSort 클래스 Sort 인터페이스 구현

--- Chapter10/src/exercise/q08/sorting/BubbleSort.java
+++ Chapter10/src/exercise/q08/sorting/BubbleSort.java
@@ -3,11 +3,19 @@ package exercise.q08.sorting;
 public class BubbleSort implements Sort {
     @Override
     public int[] ascending(int[] arr) {
+        System.out.println("BubbleSort ascending");
         return arr;
     }
 
     @Override
     public int[] descending(int[] arr) {
+        System.out.println("BubbleSort descending");
         return arr;
+    }
+
+    @Override
+    public void description() {
+        Sort.super.description();
+        System.out.println("BubbleSort 입니다.");
     }
 }
