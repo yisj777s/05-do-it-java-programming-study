7f410b7d534446cef3077918205e624c798c05d0
yisj777s <yisj777s@naver.com>
2026-09-14 19:39:01 +0900

    feat(10): 되새김 문제 Q08 HeapSort 클래스 Sort 인터페이스 구현

---
+++ Chapter10/src/exercise/q08/sorting/HeapSort.java
@@ -0,0 +1,21 @@
+package exercise.q08.sorting;
+
+public class HeapSort implements Sort {
+    @Override
+    public int[] ascending(int[] arr) {
+        System.out.println("BubbleSort ascending");
+        return arr;
+    }
+
+    @Override
+    public int[] descending(int[] arr) {
+        System.out.println("BubbleSort descending");
+        return arr;
+    }
+
+    @Override
+    public void description() {
+        Sort.super.description();
+        System.out.println("HeapSort 입니다.");
+    }
+}
